4a4b9fe4f25d952acbf34b4545d70a324e429337
sumitxsdet <sumitxsirohi01@gmail.com>
2026-07-23 11:56:27 +0530

    Initial Playwright Project

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.only('Valid Login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await page.waitForTimeout(5000); 
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
